17ace9aa4f6cb2ce1337f82d6cb43a0ca1bfa1c1
Adarsh Gautham <adarsh.gautham@mediassist.in>
2025-08-27 18:01:30 +0000

    creae a new branch publish and make publish this code as gitgub page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -8,6 +10,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
