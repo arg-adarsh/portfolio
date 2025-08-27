aed669f378aa8542ea8961aaf7276c8d43d180bd
Adarsh Gautham <adarsh.gautham@mediassist.in>
2025-08-27 18:46:26 +0000

    in bigiinig it was working fine but now it is showing portfolio Firebas

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-// IMPORTANT: Replace "your-repo-name" with the name of your GitHub repository.
-const repoName = '/portfolio';
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
+// This setup is for deploying to GitHub Pages.
+// It automatically sets the correct base path and asset prefix.
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
 
 if (isGithubActions) {
-  assetPrefix = repoName
-  basePath = repoName
+  // It's important to replace 'portfolio' with your repository name.
+  // This will be used as the base path for your assets.
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
 }
 
 const nextConfig = {
