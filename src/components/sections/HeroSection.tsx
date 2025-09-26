690bd850e30b3e3fc4d5db28293a4da45cf644df
Adarsh Raj Gautam <arg.adarsh@gmail.com>
2025-09-26 12:11:34 +0530

    Update HeroSection.tsx

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -34,8 +34,8 @@ export const HeroSection = () => {
          <Image 
             src="https://picsum.photos/800/800"
             alt="Adarsh Raj Gautam"
-            width={100}
-            height={100}
+            width={800}
+            height={800}
             className="rounded-lg shadow-2xl w-full max-w-[300px] lg:max-w-[500px]"
             data-ai-hint="professional portrait"
           />
